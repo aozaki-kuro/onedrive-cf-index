cfa49cd17f5f7e256b13be24cbaa897ac5c9bdb5
Aozaki Kuro <nihanwen@gmail.com>
2021-01-20 16:33:58 +0800

    Update intro

--- src/folderView.js
+++ src/folderView.js
@@ -52,8 +52,8 @@ export async function renderFolderView(items, path, request) {
   const intro = `<div class="intro markdown-body" style="text-align: left; margin-top: 2rem;">
                     <h2>Hoshimachi Suisei's YouTube Archive ☄</h2>
                     <p>This is an archive site for Hoshimachi Suisei's YouTube archive.</p>
-                    <p>Do not distribute this site under any circumstances.</p>
                     <p>If you find out dupilicate or paid contents uploaded, please report in the Discord server.</p>
+                    <p>Do not distribute this site under any circumstances.</p>
                     <p><b>请不要转发本站链接，谢谢合作。</b></p>
                   </div>`
 
